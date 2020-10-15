4fa838e305f5ea36bf2e9e8f93c6506d0f0d0af4
gabrieltherock <gabrieldarocha2013@hotmail.com>
2020-10-15 14:43:50 -0300

    Creating service layer and findById method

--- src/main/java/com/gabriel/web/resources/UserResource.java
+++ src/main/java/com/gabriel/web/resources/UserResource.java
@@ -1,20 +1,50 @@
 package com.gabriel.web.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.web.entities.User;
+import com.gabriel.web.services.UserService;
+
+/*
+ * Resource --> Service --> Repository
+ * A aplicação funciona em camadas, começando pela resource
+ */
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	//método que retorna todos os usuários
+	@Autowired
+	private UserService service;
+	
+	/*
+	 * O ResponseEntity é o retorno da requisição
+	 * O ok corresponde ao código HTTP de sucesso (200)
+	 * Body é o corpo da resposta, no caso um objeto
+	 * Dentro desses métodos, chamamos métodos da classe de serviço, que acessa repository
+	 */
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "gabriel", "gabriel@gmail", "11123456789", "gabriel123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	/*
+	 * No GetMapping, adiciona-se value para aceitar parâmetros no endpoint
+	 * @PathVariable sinaliza que a variável é capturada pelo endpoint
+	 */
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/gabriel/web/services/UserService.java
@@ -0,0 +1,30 @@
+package com.gabriel.web.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabriel.web.entities.User;
+import com.gabriel.web.repositories.UserRepository;
+
+/*
+ * Registra a classe como componente que pode ser injetado
+ */
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
